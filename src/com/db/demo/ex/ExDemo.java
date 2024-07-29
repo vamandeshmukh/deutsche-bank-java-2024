7c6d291bf5aa9fbabd7cd99d0bae8bd0c0397ae9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-29 10:53:51 +0530

    core java concepts

---
+++ src/com/db/demo/ex/ExDemo.java
@@ -0,0 +1,26 @@
+package com.db.demo.ex;
+
+import java.util.Scanner;
+
+public class ExDemo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter num1");
+		int num1 = sc.nextInt();
+
+		System.out.println("Enter num2 but don't enter 0");
+		int num2 = sc.nextInt();
+
+		try {
+			System.out.println(num1 / num2);
+		} catch (ArithmeticException e) {
+			System.out.println("Do not enter 0");
+		}
+
+		sc.close();
+
+	}
+}
---
+++ src/com/db/demo/io/IoDemo3.java
@@ -0,0 +1,27 @@
+package com.db.demo.io;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
+public class IoDemo3 {
+	
+	public static void main(String[] args) {
+	
+		InputStream is; // abstract class  
+		PrintStream ps;
+		Reader r; // // abstract class
+		FileInputStream fis; // 
+		FileWriter fw;
+
+//		DataInput	
+//		ObjectInput	
+//		ObjectFilter	// na 
+//		FileFilter	
+	
+		
+	}
+
+}
